79bcfa543c699933a483aa3f26bf475b34126854
QingyanSu <345885398@.qq.com>
2021-04-20 17:40:02 +0800

    V1.0.4
    1.可获取根目录下文件夹内全部文件路径
    2.测试根目录路径无法上传文件至网站
    3.获取路径函数改为获取根目录路径后可转换为绝对路径

--- 办公室.py
+++ 办公室.py
@@ -1,5 +1,6 @@
 from selenium import webdriver  #加载selenium模块
 import time
+import os
 
 #启动带插件的浏览器
 plug_path = r"C:/Users/Administrator/AppData/Local/Google/Chrome/User Data/"
@@ -9,12 +10,10 @@ option.add_argument("--user-data-dir="+plug_path)  #加载Chrome全部插件
 driver = webdriver.Chrome(chrome_options=option)  #更改Chrome默认选项
 driver.implicitly_wait(20)  #设置等待20秒钟
 driver.get(url)  #设置打开网页
-
 #切换页面
 def change_window(number):
     handles = driver.window_handles  #获取当前页面所有的句柄
     driver.switch_to.window(handles[number])
-
 #登录metamask钱包
 def sign_in_metamask(password_metamask):
     driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/div/div[1]/div[2]/div/div').click()  #点击登录键
@@ -23,7 +22,7 @@ def sign_in_metamask(password_metamask):
     driver.find_element_by_id("password").send_keys(password_metamask)  #输入密码
     driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div/button/span').click()  #点确定
     change_window(0)  #切换回主页面
-
+#创建收藏夹（未完成）
 def create_coll(pic_path,pic_name,pic_desc):
     driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[1]/div[1]/div/div[1]/div').click()
     driver.find_element_by_xpath('//*[@id="image"]').send_keys(pic_path)
@@ -31,11 +30,22 @@ def create_coll(pic_path,pic_name,pic_desc):
     #加名字重复情况处理
     driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[5]/div/div/div/form/div[3]/textarea').send_keys(pic_desc)
     driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[5]/div/div/div/form/div[4]/div').click()
+#获取根目录文件夹内照片绝对路径地址
+def get_files(pics_path):
+    listdir = os.listdir(pics_path)
+    filepath = os.getcwd()
+    allfile = []
+    for file in listdir:
+        allfile.append(filepath + '\\' + file)
+    return allfile
+
+#def add_item():
+
 
 if __name__ == "__main__":
     password_metamask = r"elysion0922"
     sign_in_metamask(password_metamask)
-    pic_path = r"E:\Opensea-Auto\图片.jpg"
+    pic_path = get_files(r"cover")[0]
     pic_name = r"123"
     pic_desc = r"123"
     create_coll(pic_path,pic_name,pic_desc)
