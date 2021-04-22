18a58f57ce986f17f8586cb7a938d3450998fdc9
QingyanSu <345885398@.qq.com>
2021-04-22 17:40:33 +0800

    V1.0.10
    1.完成excel读写部分
    
    prop按键再次无效，fuck！

--- test.py
+++ test.py
@@ -1,40 +1,126 @@
-from PIL import Image
-import xlutils
+
+from selenium import webdriver  #加载selenium模块
+from selenium import webdriver  # 引入selenium模块
+import xlrd   # 引入Excel读取模块
+import time
 import os
 
+#启动带插件的浏览器
+# 启用带插件的浏览器
+plug_path = r"C:/Users/Administrator/AppData/Local/Google/Chrome/User Data/"
+url = r"https://opensea.io/collections"
+option = webdriver.ChromeOptions()
+option.add_argument("--user-data-dir="+plug_path)  #加载Chrome全部插件
+driver = webdriver.Chrome(chrome_options=option)  #更改Chrome默认选项
+driver.implicitly_wait(20)  #设置等待20秒钟
+driver.get(url)  #设置打开网页
+
+# 切换页面
+def change_window(number):
+    handles = driver.window_handles  #获取当前页面所有的句柄
+    handles = driver.window_handles  # 获取当前页面所有的句柄
+    driver.switch_to.window(handles[number])
+#登录metamask钱包
+# 登录metamask钱包
+def sign_in_metamask(password_metamask):
+    driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/div/div[1]/div[2]/div/div').click()  #点击登录键
+    driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/div/div[1]/div[2]/div/div').click()  # 点击登录键
+    time.sleep(3)
+    change_window(1)  #切换至弹出页面
+    driver.find_element_by_id("password").send_keys(password_metamask)  #输入密码
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div/button/span').click()  #点确定
+    change_window(0)  #切换回主页面
+#创建收藏夹（未完成）
+def create_coll(pic_path,pic_name,pic_desc):
+    change_window(1)  # 切换至弹出页面
+    driver.find_element_by_id("password").send_keys(password_metamask)  # 输入密码
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div/button/span').click()  # 点确定
+    change_window(0)  # 切换回主页面
+# 创建收藏夹（未完成）
+def create_coll(pic_name,pic_desc):
+    driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[1]/div[1]/div/div[1]/div').click()
+    driver.find_element_by_xpath('//*[@id="image"]').send_keys(pic_path)
+    driver.find_element_by_xpath('//*[@id="image"]').send_keys(get_files(r"cover")[0])
+    driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[5]/div/div/div/form/div[2]/div/div[1]/input').send_keys(pic_name)
+    #加名字重复情况处理
+    # 加名字重复情况处理
+    driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[5]/div/div/div/form/div[3]/textarea').send_keys(pic_desc)
+    driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[5]/div/div/div/form/div[4]/div').click()
+#获取根目录文件夹内照片绝对路径地址
+    # 创建后还有一步操作
+# 给根目录文件夹内的全部文件拼装绝对路径
 def get_files(pics_path):
+    listdir = os.listdir(pics_path)
+    filepath = os.getcwd()
+    allfile = []
     listdir = os.listdir(pics_path)  # 定位文件夹位置
     filepath = os.getcwd()  # 当前工作目录
     allfile = []  # 定义为数组
     for file in listdir:
+        allfile.append(filepath + '\\' + file)
         allfile.append(filepath + '\\' + pics_path + '\\' + file)  # 拼装地址
     return allfile
 
-Image.MAX_IMAGE_PIXELS = 2300000000
-a = get_files('pic')
-sizes1 = []
-sizes2 = []
-for i in a:
-    sizes1.append(Image.open(i).size[0])
-    sizes2.append(Image.open(i).size[1])
+#def add_item():
 
-# 3.3.3 xlutils读取 写入 Excel 表格信息
-def fun3_3_3():
-    # file_path：文件路径，包含文件的全名称
-    # formatting_info=True：保留Excel的原格式（使用与xlsx文件）
-    workbook = xlrd.open_workbook('3_3 xlutils 修改操作练习.xlsx')
+# 定义浮点转字符函数
+def float_to_str(float_value):
+    try:
+        float_value = int(float_value)    #尝试转换为整数
+    except ValueError as e:    #文本或文本含数字情况赋值失败
+        return float_value     #即无需转换，直接返回
+    else:
+        if (float_value == int(float_value)):  #如果值为整数
+            return str(int(float_value))       #值转换为整数后转换为字符
+        else :                                 #如果值为浮点数
+            return str(float_value)            #值转换为字符
+# 创建NFT
+def add_item():
+    the_fate = '//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[1]/div[2]/a/div/a/div'
+    coll = the_fate
+    pics = get_files(r"pic")  # 完成第一个数组（图片）
+    datafile_path = r'file.xlsx'  # 表格位置
+    data = xlrd.open_workbook(datafile_path)  # 获取数据
+    table = data.sheet_by_name(r'item')  # 表格内工作表
+    ncols = table.ncols  # 定义列数
+    names = []
+    descs = []
+    prop_type = []
+    prop_name = []
+    for i in range(ncols):
+        if i == 1:
+            names = table.col_values(i)
+        if i == 2:
+            descs = table.col_values(i)
+        if i == 3:
+            prop_type = table.col_values(i)
+        if i == 4:
+            prop_name = table.col_values(i)
+    for i,j,k,l,n in zip(pics,names,descs,prop_type,prop_name):
+        driver.find_element_by_xpath(coll).click()
+        driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/div/main/div/div/section[2]/div[2]/div[3]/section/a/div').click()  # 点击"Add New Item"
+        driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section/div[2]/div/form/div[1]/div/div/div/input').send_keys(i)  # 上传图片
+        driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section/div[2]/div/form/div[2]/div/div[1]/input').send_keys(j)  # 图片名称
+        driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/div/main/div/div/section/div[2]/div/form/div[4]/textarea').send_keys(k)  # 描述
+        #prop按键无效
+        driver.find_element_by_xpath('//*[@id="__next"]/div[2]/div[1]').click()  # 增加Properties
+        driver.find_element_by_xpath('//*[@id="__next"]/div[2]/div[1]/div/div/div[2]/div/table/tbody/tr/td[1]/div/div/input').send_keys(l)  # 输入Prop_type
+        driver.find_element_by_xpath('//*[@id="__next"]/div[2]/div[1]/div/div/div[2]/div/table/tbody/tr/td[2]/div/div/input').send_keys(n)  # 增加Prop_name
+        driver.find_element_by_xpath('//*[@id="__next"]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div').click()  # 点击Save
 
-    new_workbook = copy(workbook)  # 将xlrd对象拷贝转化为xlwt对象
 
-    # 读取表格信息
-    sheet = workbook.sheet_by_index(0)
-    col2 = sheet.col_values(1)  # 取出第二列
-    cel_value = sheet.cell_value(1, 1)
-    print(col2)
-    print(cel_value)
+password_metamask = r"elysion0922"
+sign_in_metamask(password_metamask)
+pic_path = get_files(r"cover")[0]
+pic_name = r"123"
+pic_desc = r"123"
+create_coll(pic_path,pic_name,pic_desc)
+if get_files(r"cover") == []:
+    add_item()
+else:
+    pic_name = r"123"
+    pic_desc = r"123"
+    create_coll(pic_name, pic_desc)
 
-    # 写入表格信息
-    write_save = new_workbook.get_sheet(0)
-    write_save.write(0, 0, "xlutils写入！")
-
-    new_workbook.save("new_test.xls")  # 保存工作簿
+#签名xpath:'//*[@id="app-content"]/div/div[3]/div/div[3]/button[2]'
+#签名需切窗口
--- 家.py
+++ 家.py
@@ -1,5 +1,7 @@
 from selenium import webdriver  # 引入selenium模块
+from PIL import Image
 import xlrd   # 引入Excel读取模块
+from xlutils.copy import copy        #导入copy模块
 import time
 import os
 
@@ -53,12 +55,39 @@ def float_to_str(float_value):
             return str(int(float_value))       #值转换为整数后转换为字符
         else :                                 #如果值为浮点数
             return str(float_value)            #值转换为字符
+def get_pt():
+    # 提取excel有多少行，并做成数组
+    data = xlrd.open_workbook('file.xls', formatting_info=True)  # 打开xls文件,不修改原有样式
+    table = data.sheets()[1]
+    wb = copy(data)  # 利用xlutils.copy下的copy函数复制
+    ws = wb.get_sheet(1)  # 获取表单
+    num_nrows = []
+    for i in range(int(table.nrows)):
+        num_nrows.append(i)
+    #提取部分desc做成数组
+    table2 = data.sheet_by_name(r'item')  # 表格内工作表
+    ncols = table2.ncols  # 定义列数
+    desc_part = []
+    for i in range(ncols):
+        if i == 2:
+            desc_part = table2.col_values(i)
+    # 获取图片像素部分
+    Image.MAX_IMAGE_PIXELS = 2300000000
+    a = get_files('pic')
+    desc_mix = []
+    story = str(table2.col_values(8)[0])
+    for i,j in zip(a,desc_part):
+        desc_mix.append(str(j) + ' ' + str(Image.open(i).size[0]) + '  x  ' + str(Image.open(i).size[1]) + '  px  \n\n' + story)
+    # 拼装描述内容
+    for i,j in zip(num_nrows,desc_mix):
+        ws.write(i,2,j)  # 改变（0,0）的值
+    wb.save('file_do.xls')   # 保存文件
 # 创建NFT
 def add_item():
-    the_fate = '//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[1]/div[2]/a/div/a/div'
-    coll = the_fate
+    get_pt()
+    coll = '//*[@id="__next"]/div[1]/div/div/main/div/div/section[2]/div/div/div[1]/div[2]/a/div/a/div'
     pics = get_files(r"pic")  # 完成第一个数组（图片）
-    datafile_path = r'file.xls'  # 表格位置
+    datafile_path = r'file_do.xls'  # 表格位置
     data = xlrd.open_workbook(datafile_path)  # 获取数据
     table = data.sheet_by_name(r'item')  # 表格内工作表
     ncols = table.ncols  # 定义列数
@@ -90,12 +119,11 @@ def add_item():
 if __name__ == "__main__":
     password_metamask = r"elysion0922"
     sign_in_metamask(password_metamask)
-    if get_files(r"cover") == []:
-        add_item()
-    else:
-        pic_name = r"123"
-        pic_desc = r"123"
-        create_coll(pic_name, pic_desc)
+    #if get_files(r"cover") == []:
+    add_item()
 
+#pic_name = r"123"
+#pic_desc = r"123"
+#create_coll(pic_name, pic_desc)
 #签名xpath:'//*[@id="app-content"]/div/div[3]/div/div[3]/button[2]'
 #签名需切窗口
